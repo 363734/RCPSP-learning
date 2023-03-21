3dfe1e59dcf2cac73c587e49864331db5d42c63f
hverhaeghe <helene.verhaeghe27@gmail.com>
2023-03-21 17:58:37 -0400

    use with setup.py for out of pycharm usage

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='RCPSP-learning',
+    version='',
+    packages=['script', 'script.GNN', 'script.tasks', 'script.graphs', 'script.Instances'],
+    url='',
+    license='',
+    author='Helene Verhaeghe',
+    author_email='',
+    description=''
+)
