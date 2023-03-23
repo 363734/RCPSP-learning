8d40c919e5460afee0824b223a71f0e98214a166
hverhaeghe <helene.verhaeghe27@gmail.com>
2023-03-23 15:05:34 -0400

    script CC

---
+++ CCscripts/step2_CCarray_-preprocessing.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+i=$1
+
+bi=$(($i / 1000))
+
+benchlist=(
+    "j30"
+    "j60"
+    "j90"
+    "j120"
+)
+
+b=${benchlist[$bi]}
+
+i=$(($i % 1000))
+
+g=$(($i / 10))
+
+i=$(($i % 10))
+
+idxlist=(
+    1
+    2
+    3
+    4
+    5
+    6
+    7
+    8
+    9
+    10
+)
+
+idx=${idxlist[$i]}
+
+datadir="../target/datas"
+preprocessdir="../target/preprocessed"
+
+python ../script/tasks/task_preprocess_instance.py $b $g $idx
+
+
+
