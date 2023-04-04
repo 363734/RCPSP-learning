234cef6d0a4d7a9f8c918fc20a8acf3291f31027
hverhaeghe <helene.verhaeghe27@gmail.com>
2023-04-04 17:47:32 -0400

    test bug

--- script/GNN/learning.py
+++ script/GNN/learning.py
@@ -32,27 +32,30 @@ def learning(options):
     print(split)
     all_single_graphs = {}
 
+    count = 0
     for t, i, j in btch_seen:
-        name = "{}{}_{}".format(t, i, j)
-        all_single_graphs[name] = {}
-        print("Loading graph {}".format(name))
-        inst = parse_rcpsp(os.path.join(DIR_DATAS, "{}/{}.sm".format(t, name)))
-        graph = get_dgl_graph(inst, True)  # TODO check sans les trivial?
-        all_single_graphs[name]["train"] = graph
+        if count < 2:
+            count += 1
+            name = "{}{}_{}".format(t, i, j)
+            all_single_graphs[name] = {}
+            print("Loading graph {}".format(name))
+            inst = parse_rcpsp(os.path.join(DIR_DATAS, "{}/{}.sm".format(t, name)))
+            graph = get_dgl_graph(inst, True)  # TODO check sans les trivial?
+            all_single_graphs[name]["train"] = graph
 
-        d = split_instance(options.split_tag, options.split_train_test, inst,
-                           os.path.join(DIR_PREPROCESSED, all_prec_file.format(t, name, options.dataset_opts)))
-        all_single_graphs[name]["train"].add_edges(d["train"]["pos"][0], d["train"]["pos"][1])
+            d = split_instance(options.split_tag, options.split_train_test, inst,
+                               os.path.join(DIR_PREPROCESSED, all_prec_file.format(t, name, options.dataset_opts)))
+            all_single_graphs[name]["train"].add_edges(d["train"]["pos"][0], d["train"]["pos"][1])
 
-        all_single_graphs[name]["train-pos"] = dgl.graph((d["train"]["pos"][0], d["train"]["pos"][1]),
-                                                         num_nodes=graph.number_of_nodes())
-        all_single_graphs[name]["train-neg"] = dgl.graph((d["train"]["neg"][0], d["train"]["neg"][1]),
-                                                         num_nodes=graph.number_of_nodes())
+            all_single_graphs[name]["train-pos"] = dgl.graph((d["train"]["pos"][0], d["train"]["pos"][1]),
+                                                             num_nodes=graph.number_of_nodes())
+            all_single_graphs[name]["train-neg"] = dgl.graph((d["train"]["neg"][0], d["train"]["neg"][1]),
+                                                             num_nodes=graph.number_of_nodes())
 
-        all_single_graphs[name]["test-pos"] = dgl.graph((d["test"]["pos"][0], d["test"]["pos"][1]),
-                                                        num_nodes=graph.number_of_nodes())
-        all_single_graphs[name]["test-neg"] = dgl.graph((d["test"]["neg"][0], d["test"]["neg"][1]),
-                                                        num_nodes=graph.number_of_nodes())
+            all_single_graphs[name]["test-pos"] = dgl.graph((d["test"]["pos"][0], d["test"]["pos"][1]),
+                                                            num_nodes=graph.number_of_nodes())
+            all_single_graphs[name]["test-neg"] = dgl.graph((d["test"]["neg"][0], d["test"]["neg"][1]),
+                                                            num_nodes=graph.number_of_nodes())
 
     list_graph = [all_single_graphs[k]["train"] for k in all_single_graphs][:2]
     size= [(g.number_of_nodes(), g.number_of_edges()) for g in list_graph]
