1d41b4a4802b7eaad22e0c0191de0614e5059026
hverhaeghe <helene.verhaeghe27@gmail.com>
2023-04-18 14:06:35 -0400

    other bench

--- CCscripts/step9_CCarray_-run_with_prec.sh
+++ CCscripts/step9_CCarray_-run_with_prec.sh
@@ -2,7 +2,7 @@
 
 source ../../rcpsp/bin/activate
 
-model="split1_20-80_<=j120_[TO=600000_sbps=true_vsids=true]_0.001_bsf"
+model="split1_20-80_<=j90_[TO=600000_sbps=true_vsids=true]_0.001_bsf"
 threshold=0.75
 
 datadir="../target/datas"
