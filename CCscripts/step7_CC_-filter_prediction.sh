500604041a43fc163d8f0f51e5f6c9bd9fa3fc7d
hverhaeghe <helene.verhaeghe27@gmail.com>
2023-12-01 16:28:52 +0100

    switch param

--- CCscripts/step7_CC_-filter_prediction.sh
+++ CCscripts/step7_CC_-filter_prediction.sh
@@ -15,7 +15,7 @@ a=$SLURM_ARRAY_TASK_ID
 model="sp_sp-u_BEST_<=j120_[allprec_bsf_TO=3600000_sbps=true_vsids=true]_0.01_bsfLoss"
 #model="sp_sp-u_BEST_<=j120_[allprec_bsf_TO=3600000_sbps=false_vsids=false]_0.01_bsfLoss"
 
-threshold=0.50
+threshold=0.5
 
 i=0
 for b in "j30" "j60" "j90" "j120"
