b640c7fa319e20870b6f7f455162784649ff5fbc
hverhaeghe <helene.verhaeghe27@gmail.com>
2023-12-01 16:16:08 +0100

    switch param

--- CCscripts/step8_CC_-generate_ordering.sh
+++ CCscripts/step8_CC_-generate_ordering.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #SBATCH --account=def-pesantg
 #SBATCH --time=0-0:30
-#SBATCH --array=10-489#,1010-1489,2010-2489,3010-3609
+#SBATCH --array=3010-3609  #10-489,1010-1489,2010-2489,3010-3609
 #SBATCH --mem-per-cpu=1024M
 #SBATCH --mail-user=helene.verhaeghe@polymtl.ca
 #SBATCH --mail-type=ALL
