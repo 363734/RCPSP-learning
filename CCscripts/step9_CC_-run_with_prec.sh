a6c0f3c81d3a46e8835fe8531515cdcd75ac1194
hverhaeghe <helene.verhaeghe27@gmail.com>
2023-12-06 13:19:16 +0100

    switch param

--- CCscripts/step9_CC_-run_with_prec.sh
+++ CCscripts/step9_CC_-run_with_prec.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 #SBATCH --account=def-pesantg
-#SBATCH --time=0-1:30
-#SBATCH --array=10-489#,1010-1489,2010-2489,3010-3609
-#SBATCH --mem-per-cpu=1024M
+#SBATCH --time=0-10:00
+#SBATCH --array=3247,3368,3403,3491 #3010-3609  #10-489,1010-1489,2010-2489,3010-3609
+#SBATCH --mem-per-cpu=7000M
 #SBATCH --mail-user=helene.verhaeghe@polymtl.ca
 #SBATCH --mail-type=ALL
 
 source ../../rcpsp/bin/activate
 
-a=$1
-#a=$SLURM_ARRAY_TASK_ID
+#a=$1
+a=$SLURM_ARRAY_TASK_ID
 
 #model="sp_sp-b_BEST_<=j30_[allprec_bsf_TO=1000_sbps=false_vsids=false]_0.01_bsfLoss"
 model="sp_sp-u_BEST_<=j120_[allprec_bsf_TO=3600000_sbps=true_vsids=true]_0.01_bsfLoss"
