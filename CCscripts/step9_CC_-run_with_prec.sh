62c4021d8a7a52c29ee8dede496227fae13b3201
hverhaeghe <helene.verhaeghe27@gmail.com>
2023-12-06 18:42:16 +0100

    switch param

--- CCscripts/step9_CC_-run_with_prec.sh
+++ CCscripts/step9_CC_-run_with_prec.sh
@@ -15,7 +15,7 @@ a=$1
 model="sp_sp-u_BEST_<=j120_[allprec_bsf_TO=3600000_sbps=true_vsids=true]_0.01_bsfLoss"
 #model="sp_sp-u_BEST_<=j120_[allprec_bsf_TO=3600000_sbps=false_vsids=false]_0.01_bsfLoss"
 
-threshold=0.75
+threshold=0.5
 
 datadir="../target/datas"
 
