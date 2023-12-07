5f21d8cf2584b5f2f8cf0d789c2748399005fb31
hverhaeghe <helene.verhaeghe27@gmail.com>
2023-12-07 23:40:43 +0100

    switch param

--- CCscripts/step7to9_CC.sh
+++ CCscripts/step7to9_CC.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #SBATCH --account=def-pesantg
 #SBATCH --time=0-10:00
-#SBATCH --array=3258,3317,3525,3535 #3010-3609  #10-489,1010-1489,2010-2489,3010-3609
+#SBATCH --array=3010-3609  #10-489,1010-1489,2010-2489,3010-3609
 #SBATCH --mem-per-cpu=7000M
 #SBATCH --mail-user=helene.verhaeghe@polymtl.ca
 #SBATCH --mail-type=ALL
