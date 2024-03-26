7c1435705c0f002a1969df38f81dd2f09f9d25e6
hverhaeghe <helene.verhaeghe27@gmail.com>
2024-03-26 10:47:24 +0100

    wrong dir

--- CCscripts/bench_psplib_download.sh
+++ CCscripts/bench_psplib_download.sh
@@ -12,27 +12,27 @@ echo ":-: Download of PSPLIB data"
 for b in "j30" "j60" "j90" "j120"; do
   echo ":-: Download of PSPLIB data: benchmark $b"
 
-  mkdir -p $datadir/$b
+  mkdir -p $psplibdir/$b
 
-  wget -O $datadir/$b/$b.zip https://www.om-db.wi.tum.de/psplib/files/$b.sm.zip
-  unzip $datadir/$b/$b.zip -d $datadir/$b/
-  rm $datadir/$b/$b.zip
+  wget -O $psplibdir/$b/$b.zip https://www.om-db.wi.tum.de/psplib/files/$b.sm.zip
+  unzip $psplibdir/$b/$b.zip -d $psplibdir/$b/
+  rm $psplibdir/$b/$b.zip
 
-  hrsfile=$datadir/${b}hrs.sm
-  hrsfiletmp=$datadir/${b}hrstmp.sm
+  hrsfile=$psplibdir/${b}hrs.sm
+  hrsfiletmp=$psplibdir/${b}hrstmp.sm
   wget -O $hrsfiletmp https://www.om-db.wi.tum.de/psplib/files/${b}hrs.sm
   iconv -f utf-8 -t utf-8 -c $hrsfiletmp > $hrsfile
   rm $hrsfiletmp
 
   if [ $b == "j30" ]; then
-    optfile=$datadir/${b}opt.sm
-    optfiletmp=$datadir/${b}opttmp.sm
+    optfile=$psplibdir/${b}opt.sm
+    optfiletmp=$psplibdir/${b}opttmp.sm
     wget -O $optfiletmp https://www.om-db.wi.tum.de/psplib/files/${b}opt.sm
     iconv -f utf-8 -t utf-8 -c $optfiletmp > $optfile
     rm $optfiletmp
   else
-    lbfile=$datadir/${b}lb.sm
-    lbfiletmp=$datadir/${b}lbtmp.sm
+    lbfile=$psplibdir/${b}lb.sm
+    lbfiletmp=$psplibdir/${b}lbtmp.sm
     wget -O $lbfiletmp https://www.om-db.wi.tum.de/psplib/files/${b}lb.sm
     iconv -f utf-8 -t utf-8 -c $lbfiletmp > $lbfile
     rm $lbfiletmp
