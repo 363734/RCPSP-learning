d190e862e0e7803630bbd1ed83e915d03b9dba61
hverhaeghe <helene.verhaeghe27@gmail.com>
2024-04-08 23:18:21 +0900

    switch

--- CCscripts/step8_CC_-generate_ordering.sh
+++ CCscripts/step8_CC_-generate_ordering.sh
@@ -13,10 +13,12 @@ a=$1
 
 #model="sp_sp-b_BEST_<=j30_[allprec_bsf_TO=1000_sbps=false_vsids=false]_0.01_bsfLoss"
 #model="sp_sp-u_BEST_<=j60_[allprec_bsf_TO=3600000_sbps=true_vsids=true]_0.01_bsfLoss"
-model="sp_sp-u_BEST_<=j120_[allprec_bsf_TO=3600000_sbps=true_vsids=true]_0.01_bsfLoss"
+#model="sp_sp-u_BEST_<=j120_[allprec_bsf_TO=3600000_sbps=true_vsids=true]_0.01_bsfLoss"
 #model="sp_sp-u_BEST_<=j120_[allprec_bsf_TO=3600000_sbps=false_vsids=false]_0.01_bsfLoss"
 
-threshold=0.95
+model="sp_sp-u_BEST_<=j60_[allprec_bsf_ubto=3600000_TO=600000_sbps=true_vsids=true_p=0.7]_0.01_bsfLoss"
+
+threshold=0.99
 
 datadir="../target/datas"
 
