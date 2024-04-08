ca2a827ea9a785aed3667dc8b62cf2ea352f6c67
hverhaeghe <helene.verhaeghe27@gmail.com>
2024-04-09 00:10:49 +0900

    switch

--- CCscripts/step7to9_CC.sh
+++ CCscripts/step7to9_CC.sh
@@ -1,14 +1,16 @@
 #! /bin/bash
 #SBATCH --account=def-pesantg
 #SBATCH --time=0-10:00
-#SBATCH --array=3010-3609  #10-489,1010-1489,2010-2489,3010-3609
+#SBATCH --array=3010 #3010-3609  #10-489,1010-1489,2010-2489,3010-3609
 #SBATCH --mem-per-cpu=7000M
 #SBATCH --mail-user=helene.verhaeghe@polymtl.ca
 #SBATCH --mail-type=ALL
 
+echo "start"
 bash step7_CC_-filter_prediction.sh $SLURM_ARRAY_TASK_ID
-
+echo "filtering done"
 bash step8_CC_-generate_ordering.sh $SLURM_ARRAY_TASK_ID
-
+echo "ordering done"
 bash step9_CC_-run_with_prec.sh $SLURM_ARRAY_TASK_ID
+echo "run done"
 
